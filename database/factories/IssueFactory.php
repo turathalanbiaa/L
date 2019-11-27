d2569241d4e54bab45358632fb420abda7c97ba7
TurathAlanbiaa <turath.alanbiaa@gmail.com>
2019-11-27 10:06:24 +0300

    update db

--- database/factories/IssueFactory.php
+++ database/factories/IssueFactory.php
@@ -3,6 +3,7 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\Enum\IssueType;
+use App\Enum\Language;
 use Faker\Generator as Faker;
 use Website\Models\Issue;
 
@@ -10,6 +11,7 @@ $factory->define(Issue::class, function (Faker $faker) {
     return [
         'email' => $faker->email,
         'phone' => $faker->phoneNumber,
+        'lang' => Language::getRandomLanguage(),
         'title' => $faker->title,
         'content' => $faker->realText(1000, 2),
         'type' => IssueType::getRandomType(),
