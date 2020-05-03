da52ea333da65a15a75ce89359d2f41492100894
Karrar <karrar@gmail.com>
2020-05-03 16:56:56 +0300

    study course and general

--- app/Models/GeneralCourseHeader.php
+++ app/Models/GeneralCourseHeader.php
@@ -9,6 +9,10 @@ class GeneralCourseHeader extends Model
     protected $table = "general_course_headers";
     protected $primaryKey = "id";
     public $timestamps = false;
+//    public function GeneralCourse()
+//    {
+//        return $this->hasMany('app\modeles\GeneralCourse');
+//    }
     protected $fillable = [
         "id",
         "title",
