6f008814e110ba3ca066debae2280a7768888fdb
TurathAlanbiaa <turath.alanbiaa@gmail.com>
2020-06-06 13:20:38 +0300

    Emad Api

--- routes/api.php
+++ routes/api.php
@@ -10,18 +10,8 @@ use Illuminate\Http\Request;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
-/*Route::get('/v', function () {
- $info=[[
-     'version'=>1,
-     'd'=>'1',
- ]];
- return response($info,200);
-});*/
+/
+*/
 //User
 Route::get('users','UserController@index');
 Route::get('user/{id}','UserController@show');
@@ -30,21 +20,16 @@ Route::post('student-account','UserController@student_store');
 Route::put('update-user/{id}','UserController@update');
 Route::delete('delete-user/{id}','UserController@destroy');
 Route::post('credentials','UserController@credentials');
-//Course
-Route::get('general-courses','GeneralCourseController@index');
-Route::get('general-courses/{lang}','GeneralCourseController@getCoursesByLang');
-Route::post('single-general-course','GeneralCourseController@show');
-Route::post('single-study-course','StudyCourseController@show');
-
-
-Route::post('my-general-course','GeneralCourseController@MyCourses');
 Route::post('image-store','ImageController@store');
 Route::post('image-update','ImageController@updateimage');
 Route::post('my-images','ImageController@allimages');
-//Route::get('users','UserController@index');
 Route::post('get-countries','UserController@countries');
 Route::post('get-certificates','UserController@certificate');
 Route::post('last-announcement','AnnouncementController@lastAnnouncments');
 Route::post('get-all-announcment','AnnouncementController@getallAnnouncment');
 Route::post('single-announcment','AnnouncementController@show');
+
+
+
+//Lecturer
 
