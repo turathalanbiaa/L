d6647116a63b6adc1bf35af8801241a63428cfad
Emad Wahhab Al-Kabi <emadalkabi1994@gmail.com>
2020-06-10 00:16:48 +0300

    update

--- app/Http/Controllers/Api/ResponseTrait.php
+++ app/Http/Controllers/Api/ResponseTrait.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 trait ResponseTrait{
     public function simpleResponse($data = null) {
         return response()->json([
-            'data'   => $data ?? null,
+            'data'   => $data->isEmpty() ? null : $data,
             'status' => true,
             'error'  => null
         ]);
--- database/factories/LecturerFactory.php
+++ database/factories/LecturerFactory.php
@@ -15,7 +15,7 @@ $factory->define(Lecturer::class, function (Faker $faker) {
         "phone"          => $faker->phoneNumber,
         "password"       => md5($faker->password),
         "description"    => $faker->randomElement(array(null, $faker->realText(1000))),
-        "image"          => $faker->randomElement(array(null, $faker->imageUrl())),
+        "image"          => $faker->imageUrl(),
         "created_at"     => $faker->dateTimeBetween("-3 years", "now"),
         "last_login"     => null,
         "state"          => LecturerState::getRandomState(),
