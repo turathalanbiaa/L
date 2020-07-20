37b6dd257b5f16f6c7c55e4f572ec19df1491a8b
TurathAlanbiaa <turath.alanbiaa@gmail.com>
2020-07-20 12:29:36 +0300

    update review controller

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -69,6 +69,9 @@ class ReviewController extends Controller
             ->where("type",  request()->input("type"))
             ->first();
 
+        if (!$review)
+            return $this->simpleResponseWithMessage(false, "review not found");
+
         return $this->simpleResponse(new ReviewsCollection($review));
     }
 }
