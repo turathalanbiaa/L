d4cc3e337af420afd09e0e941df74fc641926509
TurathAlanbiaa <turath.alanbiaa@gmail.com>
2020-08-10 11:32:47 +0300

    update Database and Models

--- database/migrations/2020_04_26_125945_create_general_course_headers_table.php
+++ database/migrations/2020_04_26_125945_create_general_course_headers_table.php
@@ -19,7 +19,7 @@ class CreateGeneralCourseHeadersTable extends Migration
             $table->string("title");
             $table->string("description")->nullable();
             $table->string("image");
-            $table->timestamp("created_at")->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp("created_at")->default(DB::raw("CURRENT_TIMESTAMP"));
             $table->timestamp("updated_at")->default(DB::raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
         });
     }
